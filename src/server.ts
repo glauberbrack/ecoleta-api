2e04b298f1d5c3085f917df3cf8adb0fb906bbec
Glauber Brack <glauber@brack.com.br>
2020-07-16 11:07:34 -0300

    project configs on package.json and server.ts

--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,14 @@
-import express from 'express'
+import express from 'express';
+
+const app = express();
+
+app.get('/users', (request, response) => {
+    console.log('List')
+
+    response.json([
+        'Glauber',
+        'Rogério'
+    ])
+});
+
+app.listen(3333);
