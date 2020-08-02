e1bc12ed015f8af6223d9143739ce22d0a96b354
Glauber Brack <glauber@brack.com.br>
2020-08-02 20:21:28 -0300

    multer configuration

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,9 +1,13 @@
 import express from 'express';
+import multer from 'multer';
+import multerConfig from '../config/multer';
 
 import PointController from '../controllers/PointController';
 import ItemController from '../controllers/ItemController';
 
 const routes = express.Router();
+import upload = multer(multerConfig);
+
 const pointController = new PointController;
 const itemController = new ItemController;
 
